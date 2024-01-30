2e58ce83bc6d440b6008e9c2c6cdaaaa6f448d46
Ashfaqbs <ash001x@gmail.com>
2024-01-30 12:26:00 +0530

    General Commits

--- 12megaBlog-Appwrite/src/App.jsx
+++ 12megaBlog-Appwrite/src/App.jsx
@@ -1,12 +1,23 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux'
 import './App.css'
+import authService from './appwrite/auth'
 
 function App() {
 
 
   const [loading, setloading] = useState(true);
   const dispatch = useDispatch();
+
+
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData)=>{
+
+      })
+      .finally()
+  }, [])
+
   return (
     <>
       <h1>A blog with appwrite</h1>
