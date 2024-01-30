3b0aa79d7f26f25fea26f55f76863480d88c3a4e
Ashfaqbs <ash001x@gmail.com>
2024-01-30 11:57:42 +0530

    General Commits

--- 12megaBlog-Appwrite/src/appwrite/conf.js
+++ 12megaBlog-Appwrite/src/appwrite/conf.js
@@ -85,38 +85,28 @@ export class Service {
   }
 
   //file upload services
-  async uploadFile(file){
+  async uploadFile(file) {
     try {
-        return await this.bucket.createFile(
-            conf.appwriteBucketId,
-            ID.unique(),
-            file
-        )
+      return await this.bucket.createFile(conf.bucketId, ID.unique(), file);
     } catch (error) {
-        console.log("Appwrite serive :: uploadFile :: error", error);
-        return false
+      console.log("Appwrite serive :: uploadFile :: error", error);
+      return false;
     }
-}
+  }
 
-async deleteFile(fileId){
+  async deleteFile(fileId) {
     try {
-        await this.bucket.deleteFile(
-            conf.appwriteBucketId,
-            fileId
-        )
-        return true
+      await this.bucket.deleteFile(conf.bucketId, fileId);
+      return true;
     } catch (error) {
-        console.log("Appwrite serive :: deleteFile :: error", error);
-        return false
+      console.log("Appwrite serive :: deleteFile :: error", error);
+      return false;
     }
-}
+  }
 
-getFilePreview(fileId){
-    return this.bucket.getFilePreview(
-        conf.appwriteBucketId,
-        fileId
-    )
-}
+  getFilePreview(fileId) {
+    return this.bucket.getFilePreview(conf.bucketId, fileId);
+  }
 }
 
 const service = new Service();
