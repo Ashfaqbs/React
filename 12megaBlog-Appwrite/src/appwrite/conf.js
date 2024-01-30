4589cb5f6cb1f2034f62bbc745d7b9f14c1c8f6f
Ashfaqbs <ash001x@gmail.com>
2024-01-30 11:31:01 +0530

    General Commits

---
+++ 12megaBlog-Appwrite/src/appwrite/conf.js
@@ -0,0 +1,98 @@
+import conf from "../conf/conf";
+import { Client, Account, ID, Databases, Storage, Query } from "appwrite";
+
+export class Service {
+  client = new Client();
+  databases;
+  bucket;
+
+  constructor() {
+    this.client.setEndpoint(conf.appWriteURl).setProject(conf.projectId);
+
+    this.databases = new Databases(this.client);
+    this.bucket = new Storage(this.client);
+  }
+
+  async createPost({ title, slug, content, featuredImage, status, userID }) {
+    try {
+      return await this.databases.createDocument(
+        conf.databaseId,
+        conf.collectionId,
+        slug,
+        {
+          title,
+          content,
+          featuredImage,
+          status,
+          userID,
+        }
+      );
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async updatePost(slug, { title, content, featuredImage, status }) {
+    try {
+      return await this.databases.updateDocument(
+        conf.databaseId,
+        conf.collectionId,
+        slug,
+        { title, content, featuredImage, status }
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async deletePost(slug) {
+    try {
+      await this.databases.deleteDocument(
+        conf.databaseId,
+        conf.collectionId,
+        slug
+      );
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
+  async getPost(slug) {
+    try {
+      return await this.databases.getDocument(
+        conf.databaseId,
+        conf.collectionId,
+        slug
+      );
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
+
+  async getPosts(queries = [Query.equal("status", "active")]) {
+    try {
+      return await this.databases.listDocuments(
+        conf.appWriteURl,
+        conf.collectionId,
+        queries
+      );
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
+
+  //file upload services
+  async uploadFile(file) {
+    try {
+        } catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
+}
+
+const service = new Service();
+export default service;
